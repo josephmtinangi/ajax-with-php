c3c33bed275eb0bf5db19403d4690d6764a023c5
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-11-16 14:32:42 +0300

    Dummy categories

---
+++ subcategories.php
@@ -0,0 +1,23 @@
+<?php
+
+$categories = [
+	[
+		'id' => 1, 'name' => 'Furniture', 'subcategories' => [
+		 ['id' => 1, 'name' => 'Beds'],
+		 ['id' => 2, 'name' => 'Benches']
+		]
+	],
+	[
+		'id' => 2, 'name' => 'Lighting', 'subcategories' => [
+		 ['id' => 1, 'name' => 'Ceiling'],
+		 ['id' => 2, 'name' => 'Floor']
+		]
+	],
+	[
+		'id' => 3, 'name' => 'Accessories', 'subcategories' => [
+			['id' => 1, 'name' => 'Mirrors'],
+			['id' => 2, 'name' => 'Outdoor & Patio']
+		]
+	]
+];
+
