5ee59f1f0b8c1926e0ef3b03d983daf33e20a0f6
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-11-16 14:37:40 +0300

    Load subcategories

--- subcategories.php
+++ subcategories.php
@@ -21,3 +21,15 @@ $categories = [
 	]
 ];
 
+$category_id = isset($_GET['category_id']) ? (int) $_GET['category_id'] : 0;
+
+foreach ($categories as $category) {
+	if ($category['id'] == $category_id) {
+		$subcategories = $category['subcategories'];
+		foreach ($subcategories as $subcategory) {
+			echo "<option value=\"{$subcategory['id']}\">";
+			echo $subcategory['name'];
+			echo "</option>";
+		}
+	}
+}
