7bff11e0de38f587f34abc51802af3f3faa95e6b
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-11-16 13:59:21 +0300

    Use OOP approach

--- User.php
+++ User.php
@@ -1,10 +1,23 @@
 <?php
 class User {
-	public $first_name = "Joseph";
-	public $last_name = "Mtinangi";
-	private $password = "jmtinangi";
+	public $first_name;
+	public $last_name;
+	public $bio;
+
+	private $password;
+
+	public function User($first_name, $last_name, $bio)
+	{
+		$this->first_name = $first_name;
+		$this->last_name = $last_name;
+		$this->bio = $bio;
+	}
 }
 
-$user = new User();
+$first_name = 'John';
+$last_name = 'Doe';
+$bio = 'Tanto face forwards spook claymore mine dome meta-shoes stimulate rain. Sub-orbital girl fluidity Shibuya computer semiotics augmented reality media hacker. Film refrigerator bicycle computer skyscraper lights Legba savant corrupted order-flow nano-pistol. ';
+
+$user = new User($first_name, $last_name, $bio);
 
 echo json_encode($user);
